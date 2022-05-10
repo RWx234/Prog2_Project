77ed5e1bea2725241991311d7ff821144fe2b18d
Raffael Weber <raffael.weber@stud.fhgr.ch>
2022-05-10 21:07:22 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+
+# Create a Flask Instance
+app = Flask(__name__)
+
+
+# Create a route decorator
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html",user_name=name)
